ac7e66e9fd8924b8b0c5d380212058eb13558779
blakemax17 <123602152+blakemax17@users.noreply.github.com>
2023-09-18 16:35:12 -0700

    Add files via upload
    Uploading new version of rate_over_internet for the beta monitor to github

---
+++ new_rate_over_internet.ino
@@ -0,0 +1,70 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+const int pulseInputPin = 2; // Use D2 (Digital Pin 2) for pulse detection
+volatile unsigned long totalPulses = 0; // Declare totalPulses as volatile
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(10, 66, 192, 46); // Establish IP
+
+EthernetServer server(80); // Default server for HTTP
+
+void setup() {
+  pinMode(pulseInputPin, INPUT); // Set D2 (Digital Pin 2) to input
+
+  Serial.begin(9600); // Set the baud rate to 9600
+  while (!Serial) {
+    ; // Wait for Serial Monitor to open
+  }
+  Serial.println("Serial Monitor is ready.");
+
+  Ethernet.begin(mac, ip);  // Establish ethernet with previous specifications
+
+  Serial.println("Ethernet initialized.");
+  Serial.print("Server is at ");
+  Serial.println(Ethernet.localIP());
+
+  // Attach an interrupt to detect rising edges on the pulseInputPin
+  attachInterrupt(digitalPinToInterrupt(pulseInputPin), pulseRisingInterrupt, RISING);
+}
+
+void pulseRisingInterrupt() {
+  totalPulses++; // Increment the pulse count when a rising edge is detected
+}
+
+void loop() {
+  static unsigned long previousMillis = 0;
+  static unsigned long uploadMillis = 0;
+
+  // Measure and display the pulses per second every second
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= 1000) {
+    previousMillis = currentMillis;
+
+    float frequencykHz = (float)totalPulses / 1000.0; // Convert to kHz
+
+    // Print to the Serial Monitor with units
+    Serial.print("Frequency (kHz): ");
+    Serial.println(frequencykHz);
+
+    totalPulses = 0; // Reset pulse count for the next measurement interval
+  }
+
+  // Upload to the client over Ethernet every 5 seconds
+  if (currentMillis - uploadMillis >= 5000) {
+    uploadMillis = currentMillis;
+
+    float frequencykHz = (float)totalPulses / 5000.0; // Convert to kHz
+
+    EthernetClient client = server.available();
+    if (client) {
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html");
+      client.println("Connection: close");
+      client.println("Refresh: 5"); // Refresh the page every 5 seconds
+      client.println();
+      client.print(frequencykHz); // Send the frequency with units (kHz)
+      client.stop();
+    }
+  }
+}
