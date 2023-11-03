b609603ae45c94ffa2d449965edb82f3126d220c
blakemax17 <123602152+blakemax17@users.noreply.github.com>
2023-11-03 14:31:44 -0700

    Remove erroneous correction and changed file to display counts, not rates

--- counts_over_internet.ino
+++ counts_over_internet.ino
@@ -45,21 +45,21 @@ void loop() {
   if (currentMillis - previousMillis >= 1000) {
     previousMillis = currentMillis;
 
-    float frequencykHz = 0.991 * (float)totalPulses / 100.0; // Convert to kHz times 10
+    float frequencykHz = (float)totalPulses
     // Print to the Serial Monitor with units
     Serial.print("Frequency (kHz): ");
     Serial.println(frequencykHz);
 
-    totalPulses = 0; // Reset pulse count for the next measurement interval
+    // totalPulses = 0; // Reset pulse count for the next measurement interval
   }
 
   // Upload to the client over Ethernet every 5 seconds
   if (currentMillis - uploadMillis >= 5000) {
     uploadMillis = currentMillis;
 
-    float frequencykHz = 0.991 * (float)uploadPulses / 500.0; // Convert to kHz * 10, minus error
+    float frequencykHz = (float)uploadPulses;
 
-    uploadPulses = 0; // Reset upload pulse count
+    // uploadPulses = 0; // Reset upload pulse count
 
     EthernetClient client = server.available();
     if (client) {
